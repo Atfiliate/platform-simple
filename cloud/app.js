04e73a03c2d05961e966572e2e9542475e3c7a26
apex2060 <apex2060@gmail.com>
2019-08-27 17:50:23 -0600

    add icons and make a static file route

--- cloud/app.js
+++ cloud/app.js
@@ -149,7 +149,8 @@ module.exports = {
                 })
             }
             component._on = (event, fn)=>{
-                //the server does not do anything with this because there are no server interactions with the component.
+                // the server does not do anything with this because there are no server interactions with the component.
+                // we could possibly add an attribute to queue an event?
             }
             component._wrap = html=>{
                 component._import('https://the.homeschool.express/project/import/component/wrapper.js', '_wrapper').then(module=>{
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const myApp 	= require('./cloud/app.js') ;
 const app = express();
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/static'));
+
 app.get('/project/:projId/component/:component', auto.project)
 app.get('/project/component/:component', auto.project)
 
